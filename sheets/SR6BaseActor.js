75d045122428a0c8be94c422f6bb5ca7fa828a22
Zakarik <inconnu_jedi@hotmail.com>
2025-08-07 14:33:51 +0200

    Update SR6BaseActor.js

--- sheets/SR6BaseActor.js
+++ sheets/SR6BaseActor.js
@@ -1,9 +1,53 @@
 import { Shadowrun6Actor } from "/systems/shadowrun6-eden/module/Shadowrun6Actor.js";
 
+function isLifeform(obj) {
+    return obj.attributes != undefined;
+}
+function isSpiritOrSprite(obj) {
+    return obj.rating != undefined;
+}
+function isMatrixUser(obj) {
+    return obj.persona != undefined;
+}
+function isPlayer(obj) {
+    return obj.karma != undefined;
+}
+function isGear(obj) {
+    return obj.skill != undefined;
+}
+function isVehicle(obj) {
+    return obj.skill != undefined && (obj.type === "VEHICLES" || obj.type === "DRONES");
+}
+function isSpell(obj) {
+    return obj.category != undefined;
+}
+function isWeapon(obj) {
+    return ((obj.type === "WEAPON_FIREARMS" || obj.type === "WEAPON_CLOSE_COMBAT" || obj.type === "WEAPON_RANGED" || obj.type === "WEAPON_SPECIAL") &&
+        obj.dmg != undefined);
+}
+function isArmor(obj) {
+    return obj.defense != undefined;
+}
+function isComplexForm(obj) {
+    return obj.fading != undefined;
+}
+function isMatrixDevice(obj) {
+    return obj.d != undefined && (obj.type == "ELECTRONICS" || obj.type == 'CYBERWARE');
+}
 function getSystemData(obj) {
     if (game.release.generation >= 10)
         return obj.system;
     return obj.data.data;
+}
+function getActorData(obj) {
+    if (game.release.generation >= 10)
+        return obj;
+    return obj.data;
+}
+function getItemData(obj) {
+    if (game.release.generation >= 10)
+        return obj;
+    return obj.data;
 }
 /**
  * Extend the basic ActorSheet with some very simple modifications
@@ -40,5 +84,32 @@ export class SR6BaseActor extends Shadowrun6Actor {
                 system.skills["perception"].points = 1;
                 break;
         }
+    }
+    //---------------------------------------------------------
+    /*
+     * Calculate the pool when using items with assigned skills
+     */
+    _prepareItemPools() {
+        super._prepareItemPools();
+        const actorData = getActorData(this);
+        const system = getSystemData(this);
+        if (!isLifeform(system))
+            return;
+        actorData.items.forEach((tmpItem) => {
+            let item = getItemData(tmpItem);
+            let system = getSystemData(tmpItem);
+            if (tmpItem.type == "gear" && isWeapon(system)) {
+                if (system.stun == "false") {
+                    system.stun = false;
+                }
+                else if (system.stun == "true") {
+                    system.stun = true;
+                }
+                let suffix = ((item.calculated.stun ?? system.stun) === true || (item.calculated.stun ?? system.stun) === "true")
+                    ? game.i18n.localize("shadowrun6.item.stun_damage")
+                    : game.i18n.localize("shadowrun6.item.physical_damage");
+                system.dmgDef = item.calculated.dmg + suffix;
+            }
+        });
     }
 }
